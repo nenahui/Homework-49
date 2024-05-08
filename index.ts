8d4e500d1b0ffbd8e95b450c2a27dc35a3217b31
Kanat Sydykov <kanatraccoon@gmail.com>
2024-05-08 16:42:52 +0600

    Fixed index.html and index.ts

--- index.ts
+++ index.ts
@@ -1,15 +1,13 @@
 const chessBoard = (size: number) => {
-	const container = document.createElement('ul')
-	container.style.width = `${size * 20}px`
+	let emptyString = '';
 
 	for (let i = 0; i < size; i++) {
+		emptyString += '\n'
 		for (let j = 0; j < size; j++) {
-			const cell = document.createElement('li')
-			;(i + j) % 2 === 0 ? (cell.className = 'cell-black') : null
-			container.append(cell)
+			const cellBlack = (i + j) % 2 === 0;
+			emptyString += cellBlack ? '██' : '  '
 		}
 	}
-	document.body.append(container)
-}
 
-chessBoard(30);
+	console.log(emptyString)
+}
