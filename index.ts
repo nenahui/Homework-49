cb35ec99195950ee6f79966efac41b24fb325fd3
Kanat Sydykov <kanatraccoon@gmail.com>
2024-05-08 16:45:31 +0600

    Fix index.ts

--- index.ts
+++ index.ts
@@ -2,12 +2,12 @@ const chessBoard = (size: number) => {
 	let emptyString = '';
 
 	for (let i = 0; i < size; i++) {
-		emptyString += '\n'
+		emptyString += '\n';
 		for (let j = 0; j < size; j++) {
 			const cellBlack = (i + j) % 2 === 0;
-			emptyString += cellBlack ? '██' : '  '
+			emptyString += cellBlack ? '██' : '  ';
 		}
 	}
 
-	console.log(emptyString)
+	console.log(emptyString);
 }
